3b0d81f55bc6f79d068e62e93535c531f9f37f6d
barteksad <barteksadlej@gmail.com>
2022-04-05 16:41:15 +0200

    fixed decoding

--- so_emulator.asm
+++ so_emulator.asm
@@ -32,56 +32,112 @@ section .text
 
 decode_arg1:
 
-        ; decodes effective addressof arg1 from [rdi] to r8b
-        ; arg1 is encoded in bites 9, 10, 11 of [rd1] so we can AND it with 0x700
-        xor     r8, r8
-        mov     r8b, byte [rdi + 1]
-        and     r8b, 0x7
+    mov byte [rel X_REG], 5;
 
-        ; use rax for temporary lea source
-        mov     rax, [rel A_REG]
-        cmp     r8b, byte [rel A_REG_CODE]
-        je      .decoded_arg1
+    ; decodes value of arg1 from [rdi] to r8b
+    ; arg1 is encoded in bites 9, 10, 11 of [rdi] so we can take 3-th byte pf rdi and AND it with 0x7
+    mov     r8b, byte [rdi + 1]
+    and     r8b, 0x7
 
-        mov     rax, [rel D_REG]
-        cmp     r8b, byte [rel D_REG_CODE]
-        je      .decoded_arg1
+    ; use rax for temporary lea source
+    xor     rax, rax
 
-        mov     rax, [rel X_REG]
-        cmp     r8b, byte [rel X_REG_CODE]
-        je      .decoded_arg1
+    mov     al,  byte [rel A_REG]
+    cmp     r8b, byte [rel A_REG_CODE]
+    je      .decoded_arg1
 
-        mov     rax, [rel Y_REG]
-        cmp     r8b, byte [rel Y_REG_CODE]
-        je      .decoded_arg1
+    mov     al,  byte [rel D_REG]
+    cmp     r8b, byte [rel D_REG_CODE]
+    je      .decoded_arg1
 
-        mov     rax, [rel X_REG]
-        mov     rax, [rsi + rax]
-        cmp     r8b, byte [rel X_MEM_CODE]
-        je      .decoded_arg1
+    mov     al,  byte [rel X_REG]
+    cmp     r8b, byte [rel X_REG_CODE]
+    je      .decoded_arg1
 
-        mov     rax, [rel Y_REG]
-        mov     rax, [rsi + rax]
-        cmp     r8b, byte [rel Y_MEM_CODE]
-        je      .decoded_arg1
+    mov     al,  byte [rel Y_REG]
+    cmp     r8b, byte [rel Y_REG_CODE]
+    je      .decoded_arg1
 
-        mov     rax, [rel X_REG]
-        add     rax, [rel D_REG]
-        mov     rax, [rsi + rax]
-        cmp     r8b, byte [rel XpD_MEM_CODE]
-        je      .decoded_arg1
+    mov     al,  byte [rel X_REG]
+    mov     al,  byte [rsi + rax]
+    cmp     r8b, byte [rel X_MEM_CODE]
+    je      .decoded_arg1
 
-        mov     rax, [rel Y_REG]
-        add     rax, [rel D_REG]
-        mov     rax, [rsi + rax]
-        cmp     r8b, byte [rel YpD_MEM_CODE]
-        je      .decoded_arg1
+    mov     al,  byte [rel Y_REG]
+    mov     al,  byte [rsi + rax]
+    cmp     r8b, byte [rel Y_MEM_CODE]
+    je      .decoded_arg1
 
-        .decoded_arg1:
+    mov     al,  byte [rel X_REG]
+    add     al,  byte [rel D_REG]
+    mov     al,  byte [rsi + rax]
+    cmp     r8b, byte [rel XpD_MEM_CODE]
+    je      .decoded_arg1
 
-        mov     r8, rax ; we only care about r8b value
-        ret
+    mov     al,  byte [rel Y_REG]
+    add     al,  byte [rel D_REG]
+    mov     al,  byte [rsi + rax]
+    cmp     r8b, byte [rel YpD_MEM_CODE]
+    je      .decoded_arg1
+
+    .decoded_arg1:
+
+    mov     r8b, al ; we only care about r8b value
+    ret
         
+decode_arg2:
+
+    ; decodes value of arg2 from [rdi] to r9b
+    ; arg1 is encoded in bites 12, 13, 14 of [rdi] so we can shr rdi 11 bites and AND it with 0x7
+    mov     r9, [rdi]
+    shr     r9, 11
+    and     r9b, 0x7
+
+    ; use rax for temporary lea source
+    xor     rax, rax
+
+    mov     al,  byte [rel A_REG]
+    cmp     r9b, byte [rel A_REG_CODE]
+    je      .decoded_arg2
+
+    mov     al,  byte [rel D_REG]
+    cmp     r9b, byte [rel D_REG_CODE]
+    je      .decoded_arg2
+
+    mov     al,  byte [rel X_REG]
+    cmp     r9b, byte [rel X_REG_CODE]
+    je      .decoded_arg2
+
+    mov     al,  byte [rel Y_REG]
+    cmp     r9b, byte [rel Y_REG_CODE]
+    je      .decoded_arg2
+
+    mov     al,  byte [rel X_REG]
+    mov     al,  byte [rsi + rax]
+    cmp     r9b, byte [rel X_MEM_CODE]
+    je      .decoded_arg2
+
+    mov     al,  byte [rel Y_REG]
+    mov     al,  byte [rsi + rax]
+    cmp     r9b, byte [rel Y_MEM_CODE]
+    je      .decoded_arg2
+
+    mov     al,  byte [rel X_REG]
+    add     al,  byte [rel D_REG]
+    mov     al,  byte [rsi + rax]
+    cmp     r9b, byte [rel XpD_MEM_CODE]
+    je      .decoded_arg2
+
+    mov     al,  byte [rel Y_REG]
+    add     al,  byte [rel D_REG]
+    mov     al,  byte [rsi + rax]
+    cmp     r9b, byte [rel YpD_MEM_CODE]
+    je      .decoded_arg2
+
+    .decoded_arg2:
+
+    mov     r9b, al ; we only care about r9b value
+    ret
 
 so_emul:
 
